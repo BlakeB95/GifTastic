cccabf4e2ff41523ca12f6513c36425d0cabdfd5
Blake Bennett <blakebennettc@gmail.com>
2019-05-29 20:44:51 -0500

    can now pause gifs, spaces accounted for in search query

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     var topics = [];
-    topics.push("lion", "dog", "parrot");
+    topics.push("Lion", "Dog", "Parrot", "Hamster", "Snake", "Clown Fish", "Monkey");
 
     for(var i=0;i<topics.length;i++){
         addButton(topics[i]);
@@ -19,7 +19,7 @@ $(document).ready(function() {
 
     //clicking an animal button populates the imageGroup div with giphy images
     $(document).on("click", ".animalButton", function(){
-        var animal = $(this).text();
+        var animal = $(this).text().split(' ').join('+');
         console.log(animal);
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 
         animal + "&api_key=Gy0C0GKgoLF2GsszMkMARDq3BuaqNqZQ&limit=11";
@@ -34,7 +34,7 @@ $(document).ready(function() {
             console.log(results);
             for (var i = 0; i < results.length; i++) {
                 if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-                    $("#imageGroup").append("<div class='col-md-auto'><img class='animalImg' src='"+results[i].images.fixed_height.url+"' title='"+results[i].title+"'></img><p>Rating: "+results[i].rating+"</p></div>");
+                    $("#imageGroup").append("<div class='col-md-auto'><img class='animalImg' src='"+results[i].images.fixed_height_still.url+"' data-still='"+results[i].images.fixed_height_still.url+"' data-animate='"+results[i].images.fixed_height.url+"' data-state='still' title='"+results[i].title+"'></img><p>Rating: "+results[i].rating+"</p></div>");
                 }
             }
             })
@@ -43,8 +43,15 @@ $(document).ready(function() {
         //pausing on a dynamically created element
         $(document).on("click", ".animalImg", function(){
             console.log("ah");
-        })
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+              } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+              }
     })
-
+})
 
 
